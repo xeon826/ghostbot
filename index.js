71b66fbbcd4b4f74367f1df1231bfabee2e3fae6
josh581 <josh@deltstl.com>
2018-07-14 21:30:31 -0500

    regex fix

--- index.js
+++ index.js
@@ -14,36 +14,19 @@ const colors = require('irc-colors');
 */
 // comment
 
-const FACES = [
-    "(ц｀ω´ц*)",
-    "“ψ(｀∇´)ψ",
-    "ψ(*｀ー´)ψ",
-    "Ψ(｀▽´)Ψ",
-    "Ψ(｀◇´)Ψ",
-    "(屮｀∀´)屮",
-    "Щ(･｀ω´･Щ)",
-    "Ψ(￣∀￣)Ψ",
-    "Ψ(☆ｗ☆)Ψ",
-    "Ψ( ●｀▽´● )Ψ",
-    "ψ（｀Д´）ψ",
-    "ლ(｀∀´ლ)",
-    "＜(●｀∀´●)＞”",
-    "o(○｀ω´○)9",
-    "ρ(｀.´)ρ",
-    "पुनः कदा मेलिष्यामः ?"
-];
 
 const TAYTAYMSGS = [
-    "your words always lift my spirits, taytay",
-    "i've been dying to speak to you again, dear",
-    "my longing for you is supernatural, baby",
-    "just one more night. No wine, no BOOs",
-    "i'd ask you out to a bar sometime, but they'd just say \"no spirits\"",
-    "just make an album about me pls and i wont make any more BOOring puns",
-    "the washington ghost says that you and i should get together",
-    "remember the time when we were at blockbusters, and they said they had bambi, and i said bamBOO?",
-    "i recently published a biography about you, but i used a ghostwriter",
-    "did you hear about the party installgen2 was throwing? he said anything GHO-you know what this was a shitty pun"
+  "your words always lift my spirits, taytay",
+  "i've been dying to speak to you again, dear",
+  "my longing for you is supernatural, baby",
+  "just one more night. No wine, no BOOs",
+  "i'd ask you out to a bar sometime, but they'd just say \"no spirits\"",
+  "just make an album about me pls and i wont make any more BOOring puns",
+  "the washington ghost says that you and i should get together",
+  "remember the time when we were at blockbusters, and they said they had bambi, and i said bamBOO?",
+  "i recently published a biography about you, but i used a ghostwriter",
+  "i died so I could haunt you.",
+  "did you hear about the party installgen2 was throwing? he said anything GHO-you know what this was a shitty pun"
 ];
 
 /*
@@ -51,29 +34,31 @@ const TAYTAYMSGS = [
 */
 
 function randomFromArray(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
+  return arr[Math.floor(Math.random() * arr.length)];
 }
 
 function writeTombstone(bot, to, victim) {
-    let second = "  ☆ | R.I.P.  ||  ";
-    let third = "    | ~----~  ||  ";
-    
-    if (typeof victim == "undefined") {
-        second += "       ☆ ";
-        third += "*";
-    } else {
-        second += "HERE LIES ";
-        third += victim;
-    }
-    
-    [
-        "     /￣￣￣￣\\\\ ☆      *", // extra \s to because \ is an escape character
-        second,
-        third,
-        "――٩―|________ ||✿｡――――/―ノ―――ヾ―――"
-    ].forEach((line, i) => {
-        bot.say(to, line);
-    });
+  let second = colors.navy.bgblack(" ☆ ") + colors.black.bgwhite(" R.I.P. ║");
+  let third = colors.cyan.bgblack("      ") + colors.black.bgwhite("  ║");
+
+  if (typeof victim == "undefined") {
+    second += colors.navy.bgblack("       ☆ ");
+    third += colors.cyan.bgblack("*          ");
+  } else {
+    second += colors.cyan.bgblack("  HERE LIES ");
+    third += colors.cyan.bgblack("     " + victim + "       ");
+  }
+
+  [
+    //TODO make tombstone A E S T H I C
+    colors.cyan.bgblack("      ") + colors.black.bgwhite("  ║") + colors.purple.bgblack("         ☆     "),
+    second,
+    third,
+    colors.cyan.bgteal("    ") + colors.cyan.bgteal("  ") + colors.black.bgwhite("  ║") + colors.cyan.bgteal("               "),
+    colors.cyan.bgteal("                        ")
+  ].forEach((line, i) => {
+    bot.say(to, line);
+  });
 }
 
 /*
@@ -81,83 +66,82 @@ function writeTombstone(bot, to, victim) {
 */
 
 {
-    let bot = new IrcClient(CONFIG.server, CONFIG.nick, CONFIG.connection);
+  let bot = new IrcClient(CONFIG.server, CONFIG.nick, CONFIG.connection);
 
-    bot.addListener("message", (nick, to, message) => {
-        if (nick == "taylorswift" && Math.floor(Math.random()*25) == 24 && message.toLowerCase().indexOf("youtube") == -1 && message.toLowerCase().indexOf("[url]") == -1) {// 1/25 chance of replying to taylorswift. TODO: get taylorswift to always respond with "die"
-            bot.say(to, `${nick}: ${randomFromArray(TAYTAYMSGS)} ;)`);
-            return;
-        }
-            
-        if (CONFIG.ignoreList.indexOf(nick) > -1) {
-            return;
-        }
-        
-        let ripRegex = /rip (.+)/.exec(message);
-        if (ripRegex != null) {
-            writeTombstone(bot, to, ripRegex[1]);
-            return;
-        }
-        
-        
-        
-        if (message.toLowerCase() == ".bots") {
-            bot.say(to, CONFIG.dotbots);
-            return;
-        }
-        
-        let res = "";
-        message.split(" ").forEach((element) => {
-            let word = element.toLowerCase().replace(/[^_-z]+/g, ""); // this limits words to being a-z, while still including '_' and '`'
-            
-            switch (word) {
-                case "spooky":
-                case "scary":
-                case "skeletons":
-                    res = randomFromArray(FACES);
-                    break;
-                case "kys":
-                case "kms":
-                case "dead":
-                    res = "／(x~x)＼";
-                    break;
-                case "jesus":
-                    res = `${nick}: jeebus *`;
-                case "holy":
-                case "god":
-                    res = "ゞ◎Д◎ヾ धावनं करोति { कृ }";
-                    break;
-                case "ghost_bot":
-                    res = "(((╬  ╬) ईङ्खति ";
-                    break;
-            }
-        });
-        if (res != "")
-                bot.say(to, res);
-    });
+  bot.addListener("message", (nick, to, message) => {
+    if (nick == "tay" && Math.floor(Math.random() * 25) == 24 && message.toLowerCase().indexOf("youtube") == -1 && message.toLowerCase().indexOf("[url]") == -1) { // 1/25 chance of replying to taylorswift. TODO: get taylorswift to always respond with "die"
+      bot.say(to, `${nick}: ${randomFromArray(TAYTAYMSGS)} ;)`);
+      return;
+    }
 
-    bot.addListener("ctcp-version", (nick) => {
-        bot.notice(nick, "\u0001VERSION ayylmao\u0001");
-    });
+    if (CONFIG.ignoreList.indexOf(nick) > -1) {
+      return;
+    }
 
-    bot.on("error", (message) => {
-        console.log(message);
-        return;
-    });
-    
-    bot.on("motd", function(motd) {
-        console.log(motd);
-    });
+//    let ripRegex = /^rip (.+)/.exec(message);
+    let ripRegex = /^rip\s+(?:i\w+\s+p\w+\s+)?(\S{1,25})/.exec(message);
+    if (ripRegex != null) {
+      writeTombstone(bot, to, ripRegex[1]);
+      return;
+    }
 
-    bot.on("registered", () => {
-        if (CONFIG.nickserv.enabled) {
-            bot.say("NickServ", `IDENTIFY ${CONFIG.nickserv.password}`);
-        }
-    });
 
-    bot.on("invite", (channel, nick) => {
-        bot.join(channel);
-        bot.say(channel, `${nick} here lies ghost bot, rip jones McCucky!`);
-        return;
+
+    if (message.toLowerCase() == ".bots") {
+      bot.say(to, CONFIG.dotbots);
+      return;
+    }
+
+    let res = "";
+    message.split(" ").forEach((element) => {
+      let word = element.toLowerCase().replace(/[^_-z]+/g, ""); // this limits words to being a-z, while still including '_' and '`'
+
+      switch (word) {
+        case "spooky":
+        case "scary":
+        case "skeletons":
+        case "boo":
+          res = randomFromArray(EMOTE.FEAR);
+          break;
+        case "kys":
+        case "kms":
+        case "dead":
+          res = "／(x~x)＼";
+          break;
+        case "jesus":
+          res = `${nick}: jeebus *`;
+          break;
+        case "ghost_bot":
+          res = '(･_├┬┴┬┴┬';
+          break;
+      }
     });
+    if (res != "")
+      bot.say(to, res);
+  });
+
+  bot.addListener("ctcp-version", (nick) => {
+    bot.notice(nick, "\u0001VERSION ayylmao\u0001");
+  });
+
+  bot.on("error", (message) => {
+    console.log(message);
+    return;
+  });
+
+  bot.on("motd", function(motd) {
+    console.log(motd);
+  });
+
+  bot.on("registered", () => {
+    if (CONFIG.nickserv.enabled) {
+      bot.say("NickServ", `IDENTIFY ${CONFIG.nickserv.password}`);
+    }
+  });
+
+  bot.on("invite", (channel, nick) => {
+    bot.join(channel);
+    bot.say(channel, `${nick} here lies ghost bot, rip dan!`);
+    return;
+  });
 }
